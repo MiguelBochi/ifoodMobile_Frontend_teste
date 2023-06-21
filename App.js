da7523056bb09481e49ad50761593cae85ab96b3
MiguelBochiBarros <miguelbochibarro@gmail.com>
2023-06-21 16:48:47 -0300

    Atualização 3 mudança total

--- App.js
+++ App.js
@@ -1,11 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  Text,
+  View,
+  StyleSheet,
+  Image,
+  ScrollView,
+  FlatList,
+} from "react-native";
+import Header from "./src/components/Header";
 
 export default function App() {
+  const comidas = [
+    {
+      foto: "",
+      loja: "Padaria doce Lar",
+      nome: "Pâo de Quiejo",
+      preco: 3.0,
+      numero: "Unidade",
+    },
+    {
+      foto: "",
+      loja: "Quiosque São Alberto",
+      nome: "Coxinha",
+      preco: 5.0,
+      numero: "Unidade",
+    },
+    {
+      foto: "",
+      loja: "Marcado Boa Vida",
+      nome: "Tomate",
+      preco: 12.0,
+      numero: "KG",
+    },
+    {
+      foto: "",
+      loja: "Padaria doce Lar",
+      nome: "bolo de chocolate",
+      preco: 10.0,
+      numero: "unidade",
+    },
+  ];
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      < Header />
+      <ScrollView>
+        
+        <FlatList data={comidas} renderItem={({item}) => <Text>{props.comidas.nome}</Text> } />
+      </ScrollView>
     </View>
   );
 }
@@ -13,8 +54,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
